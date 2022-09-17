cb4f07f2b456da5107cc4f099421f766c0f193ba
Jess <jessjelardo@gmail.com>
2022-09-18 01:01:06 +0800

    reverting commit

--- src/Components/CardList.js
+++ src/Components/CardList.js
@@ -5,10 +5,6 @@ const CardList = ({ robots }) => {
     const cardComponent = robots.map((user, i) => { //map = for loop(ES6)
         return <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
     })
-
-    if (true){
-        throw new Error("Error display");
-    }
     return (
         <div>
             {cardComponent}
